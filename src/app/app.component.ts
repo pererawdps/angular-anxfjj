b6d5eeacb3ff8a326ea4707b78cf46698c3597ce
pererawdps <pererawdps@gmail.com>
2021-09-08 07:19:00 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+import { CUSTOMERS } from './customers';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  customers = CUSTOMERS;
+  currentCustomer = 'Maria';
+
+  title = 'Featured product:';
+  itemImageUrl = '../assets/potted-plant.png';
+
+  recommended = 'You might also like:';
+  itemImageUrl2 = '../assets/lamp.png';
+
+
+
+  getVal(): number { return 2; }
+
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
